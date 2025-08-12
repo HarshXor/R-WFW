e8673372a92a54b6830538e27a2fa9151c90d3a0
Afrizal F.A <77115748+HarshXor@users.noreply.github.com>
2025-08-12 13:54:16 +0700

    Update get.php

--- app/config/Routes/test/get.php
+++ app/config/Routes/test/get.php
@@ -5,7 +5,7 @@
 
 $route->Middleware(
     middleware: 'Logger',
-    constructParams: ['../tmp/app.log', 2097152, ['info', 'error']],
+    constructParams: ['../tmp/' . date(format: "Y-m-d") . '.log', 2097152, ['info', 'error']],
     function: 'write',
     params: ['Incoming request', 'info', [200, 512]]
 );
